b86931484d426580ccc6317d90fab9c0371b62b8
GameDev <game@local>
2026-07-14 14:24:35 +0800

    clean up junk files

--- write_game.py
+++
@@ -1,4 +0,0 @@
-﻿import sys
-sys.stdout.reconfigure(encoding='utf-8')
-content = open(r"C:\Users\30539\Documents\keji\parkour-game-new.html", "r", encoding="utf-8").read()
-print("OK: " + str(len(content)) + " chars")
